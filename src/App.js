2284c5bff7b2b12f51dff05542bcf5412df9d2a8
headring <myhakiki@gmail.com>
2022-09-16 15:18:07 +0900

    main page 작성(?)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { Button } from './components/button';
+import { useState } from 'react';
+
+const Background = styled.div`
+  width: 100vw; 
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const IphoneWrapper = styled.div`
+  width: 390px;
+  height: 844px;
+  background-color: gray;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Timeform = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
 
 function App() {
+  const [min, setMin] = useState('00')
+
+  const submitTime = (e) =>{
+    setMin(e.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Background>
+      <IphoneWrapper>
+        <h1>TimeTimer</h1>
+        <Timeform onSubmit={submitTime}>
+          <Button>Set Time</Button>
+          <input type='string' value={min} onChange={(e) => setMin(e.target.value)} />
+        </Timeform>
+
+        
+      </IphoneWrapper>
+    </Background>
+);
 }
 
 export default App;
