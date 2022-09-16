f0dcd81a3c8d4bf1c84ea8f44368ca65bcbd5c63
headring <myhakiki@gmail.com>
2022-09-16 15:16:50 +0900

    button 컴포넌트 생성

---
+++ src/components/button.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  width: 274px;
+  height: 60px;
+  background-color: yellow;
+  margin-top: 11px;
+  margin-bottom: 11px; 
+`
+
